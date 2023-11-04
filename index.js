2ed18550e674001f8bad8eb67b2434914348a580
Tobi <babalolatobiloba4real@gmail.com>
2023-11-04 16:34:30 +0200

    updated page

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post("/failure", function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
     console.log("server is running on port 3000");
 })
 
