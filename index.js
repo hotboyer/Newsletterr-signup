27496b846cce51786571794ca0564816fe9e9ec6
Tobi <babalolatobiloba4real@gmail.com>
2023-11-06 10:00:59 +0200

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/signup.html"));
+    res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function (req, res) {
